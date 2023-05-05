32424f45a314a830a38b952f0a8efb16d128a89c
TTtie <me@tttie.cz>
2023-05-05 14:38:10 +0000

    fix up a few more bugs in the stage 2 install script

--- install_archlinux_aarch64_rpi/stage2.sh
+++ install_archlinux_aarch64_rpi/stage2.sh
@@ -1,4 +1,10 @@
-pacman -S cage nano flatpak networkmanager
+#!/bin/bash
+pacman-key --init
+pacman-key --populate archlinuxarm
+
+# Update the system and then install additional packages
+pacman -Syu
+pacman -S cage flatpak networkmanager # nano is bundled in the default Arch ARM install
 
 
 useradd -m kiosk
